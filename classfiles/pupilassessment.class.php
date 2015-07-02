7037a872b7a863aeaf26908bb05ebefc0ca9e845
John Welch <jwelch@welchitconsulting.co.uk>
2015-07-02 13:44:26 +0100

    Added pupil list to the front of the assessment questionnaire

--- classfiles/pupilassessment.class.php
+++ classfiles/pupilassessment.class.php
@@ -23,10 +23,76 @@
 require_once($CFG->dirroot . '/mod/sliclquestions/classfiles/sliclquestions.class.php');
 require_once($CFG->dirroot . '/mod/sliclquestions/classfiles/student.class.php');
 
-class sliclquestions_pupil_assessment extends sliclquestions
+class sliclquestions_pupil_assessment
 {
+    static private $_instance;
+
+    static public function get_instance(&$course, $context, &$survey, $url, $params)
+    {
+        if (empty(self::$_instance)) {
+            self::$_instance = new mod_sliclquestions_pupil_register($course, $context, $survey, $url, $params);
+        }
+        return self::$_instance;
+    }
+
     public function __construct(&$course, &$cm, $id = 0, &$sliclquestions = null, $addquestions = null)
     {
-        parent::__construct($course, $cm, $id, $sliclquestions, $addquestions);
+        $this->display_pupils($survey, $url, $params);
+    }
+
+    private function display_pupils(&$survey, &$url, &$params)
+    {
+        global $DB, $USER, $OUTPUT;
+
+        // Define the table of pupils
+        $table = new html_table();
+        $table->head = array(get_string('tblname', 'sliclquestions'),
+                             get_string('tblsex', 'sliclquestions'),
+                             get_string('tblyear', 'sliclquestions'),
+                             get_string('tblclass', 'sliclquestions'),
+                             get_string('tblcommand', 'sliclquestions'));
+        $table->align = array('left',
+                              'center',
+                              'center',
+                              'center',
+                              'center');
+        $sql = 'SELECT * FROM {sliclquestions_students} WHERE survey_id=? '
+             . 'AND teacher_id=? AND deleteflag=0 ORDER BY sex DESC, kpi_level '
+             . 'ASC, surname ASC, forename ASC';
+        $pupils = $DB->get_records_sql($sql, array($survey->id, $USER->id));
+        if ($pupils) {
+            foreach($pupils as $pupil) {
+                $assessurl = $url;
+                $assessurl->params(array('act' => 'assess',
+                                         'pid' => $pupil->id));
+                $assessbtn = '<a href="' . $editurl . '">' . get_string('edit') . '</a>';
+                $table->data[] = array($pupil->forename . ' ' . $pupil->surname,
+                                       ($pupil->sex == 'm' ? get_string('male', 'sliclquestions')
+                                                           : get_string('female', 'sliclquestions')),
+                                       $pupil->year_id,
+                                       $pupil->class_id,
+                                       $assessbtn);
+            }
+        } else {
+            // No pupils registered
+        }
+
+        $options = (empty($survey->displayoptions) ? array() : unserialize($survey->displayoptions));
+
+        $content = file_rewrite_pluginfile_urls($survey->content, 'pluginfile.php', $survey->context->id, 'mod_sliclquestions', 'content', $survey->id);
+        $formatopt              = new stdClass();
+        $formatopt->noclean     = true;
+        $formatopt->overflowdiv = true;
+        $formatopt->context     = $survey->context;
+
+        // Output the list of pupils
+        echo $OUTPUT->header()
+           . $OUTPUT->heading(format_text($survey->name))
+           . ((!empty($options->printintro) && trim( strip_tags($survey->intro))) ? $OUTPUT->box(format_module_intro('sliclquestions', $survey, $survey->cm->id), 'mod_introbox', 'intro') : '')
+           . $OUTPUT->box(format_text($survey->content, $survey->contentformat, $formatopt), 'generalbox center clearfix')
+           . $OUTPUT->box_start('generalbox center clearfix')
+           . html_writer::table($table)
+           . $OUTPUT->box_end()
+           . $OUTPUT->footer();
     }
 }
