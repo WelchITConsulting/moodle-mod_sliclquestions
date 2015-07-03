25600608702e6434f7faa7b95cafc3b977ebc93c
John Welch <jwelch@welchitconsulting.co.uk>
2015-07-03 11:02:40 +0100

    Yet more debugging

--- classfiles/sliclquestions.class.php
+++ classfiles/sliclquestions.class.php
@@ -77,6 +77,7 @@ class sliclquestions
             foreach($records as $record) {
 echo '<pre>Record: ' . print_r($record, true) . '</pre>';
                 $this->questions[$record->id] = new sliclquestions_question(0, $record, $this->context);
+die('<pre>Record: ' . print_r($record, true) . '</pre>');
                 if ($record->type_id != SLICLQUESPAGEBREAK) {
                     $this->questionsbysec[$sec][$record->id] = &$this->questions[$record->id];
                     $isbreak = false;
