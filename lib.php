84b26d51cb831bfe4b48bcbdb26391a3b56f236b
John Welch <jwelch@welchitconsulting.co.uk>
2015-07-08 21:01:54 +0100

    Updated module features to allow groups

--- lib.php
+++ lib.php
@@ -33,8 +33,8 @@ function sliclquestions_supports($feature)
         case FEATURE_COMPLETION_TRACKS_VIEWS:   return false;
         case FEATURE_GRADE_HAS_GRADE:           return false;
         case FEATURE_GRADE_OUTCOMES:            return false;
-        case FEATURE_GROUPS:                    return false;
-        case FEATURE_GROUPINGS:                 return false;
+        case FEATURE_GROUPS:                    return true;
+        case FEATURE_GROUPINGS:                 return true;
         case FEATURE_MOD_INTRO:                 return true;
         case FEATURE_SHOW_DESCRIPTION:          return true;
 
