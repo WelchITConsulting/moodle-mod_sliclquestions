55946998b02e44e05db617dc3107a298cb72b184
John Welch <jwelch@welchitconsulting.co.uk>
2015-07-13 12:35:23 +0100

    Debugging javascript url processing

--- module.js
+++ module.js
@@ -99,12 +99,12 @@ M.mod_sliclquestions.init_sendmessage = function(Y) {
 }
 M.mod_sliclquestions.init_reportfilters = function(Y) {
     Y.one('.pupil-sex').delegate('click', function(e){
-//        url = processUrl();
-//        alert(url);
+        url = processUrl();
+        alert(url);
     }, 'input[type=radio]');
     Y.one('.school-year').delegate('click', function(e){
-//        url = processUrl();
-//        alert(url);
+        url = processUrl();
+        alert(url);
     }, 'input[type=radio]');
 }
 function processUrl()
@@ -113,14 +113,15 @@ function processUrl()
     if (match[6] != '') {
         params = match[6].substr(1).split('&');
         for(i=0;i<params.length;+i) {
-            if (params[i].substr(0,2) == 'x=') {
-                params[i] = 'x=' + document.getElementByName('x').valueOf();
-            }
-            if (params[i].substr(0,2) == 'y=') {
-                $params[i] = 'y=' + document.getElementByName('y').valueOf();
-            }
+alert(params[i]);
+//            if (params[i].substr(0,2) == 'x=') {
+//                params[i] = 'x=' + document.getElementByName('x').valueOf();
+//            }
+//            if (params[i].substr(0,2) == 'y=') {
+//                $params[i] = 'y=' + document.getElementByName('y').valueOf();
+//            }
         }
-        match[6] = '?' + params.join('&');
+//        match[6] = '?' + params.join('&');
     }
-    return match[1] + '//' + match[2] + '/' + match[5] + match[6] + match[7];
+    return match[1] + '//' + match[2] + '/' + match[5];// + match[6] + match[7];
 }
